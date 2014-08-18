15fbe4a832c612d57885cfb956919ffd94facf24
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-18 11:44:11 +0800

    sync daily version 2.0.1

---
+++ 2.0.1/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/cityselector/2.0.1/linkage': { requires: ['rich-base', 'node', 'event']} 
+});
---
+++ 2.0.1/meta/alias.js
@@ -0,0 +1,3 @@
+config({
+    'kg/cityselector/index': {alias: ['kg/cityselector/2.0.0/index']}
+});
